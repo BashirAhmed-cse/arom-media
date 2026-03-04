bad122dcb1caf4439cb6827dccc7ff9795f8b00d
Bashir <bashirahmed.cse@gmail.com>
2026-03-04 19:20:01 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // output: 'export',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // Allow all paths from Cloudinary
+      },
+      // Add other domains if needed:
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.example.com', // Wildcard subdomains
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
 };
 
 export default nextConfig;
